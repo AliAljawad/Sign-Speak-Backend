58fb861d1fbd56ea3fc5010ef8b92694bbdaa9cd
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-30 20:23:46 +0300

    added timestamps to voices table migration

--- database/migrations/2024_08_30_151722_create_voices_table.php
+++ database/migrations/2024_08_30_151722_create_voices_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('name');
             $table->string('eleven_labs_voice_Id');
+            $table->timestamps();
         });
     }
 
