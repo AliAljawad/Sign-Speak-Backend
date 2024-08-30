34d1b74358bfb78bd1a2e4058d8c4dc1171d71b5
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-30 19:47:07 +0300

    created translaionfactory

---
+++ database/factories/TranslationFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Translation>
+ */
+class TranslationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'voice_id' => Voice::factory(), 
+            'input_type' => $this->faker->randomElement(['video', 'image', 'live']),
+            'input_data' => $this->faker->filePath(), 
+            'translated_text' => $this->faker->sentence(),
+            'translated_audio' => $this->faker->filePath(), 
+            'translation_date' => now(),
+        ];
+    }
+}
