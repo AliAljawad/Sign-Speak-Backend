8200f0574c3fe81e761111d58690d70367e08403
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-04 17:24:39 +0300

    added API endpoint to retrieve all users in UserController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
@@ -9,3 +10,5 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
+
+Route::get('getusers', [UserController::class, 'index']);
