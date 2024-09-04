b92c330e658a0bb00da46ffd776e1b9b849fdbd2
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-04 17:27:04 +0300

    added API endpoint to retrieve a single user by ID

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,4 @@ Route::controller(AuthController::class)->group(function () {
 Route::get('getusers', [UserController::class, 'index']);
 Route::Post('adduser', [UserController::class, 'store']);
 Route::put('updateuser', [UserController::class, 'update']);
+Route::get('getuser', [UserController::class, 'show']);
