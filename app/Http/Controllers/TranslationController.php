8ae4ec2f843c0da6226dca63613c36065022d00e
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-04 18:10:32 +0300

    added  store method to TranslationController

--- app/Http/Controllers/TranslationController.php
+++ app/Http/Controllers/TranslationController.php
@@ -21,7 +21,26 @@ class TranslationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'voice_id' => 'nullable|exists:voices,id',
+            'input_type' => 'required|in:video,image,live',
+            'input_data' => 'nullable|string',
+            'translated_text' => 'required|string',
+            'translated_audio' => 'nullable|string',
+        ]);
+
+        // Create a new translation record
+        $translation = Translation::create([
+            'user_id' => $request->user_id,
+            'voice_id' => $request->voice_id,
+            'input_type' => $request->input_type,
+            'input_data' => $request->input_data,
+            'translated_text' => $request->translated_text,
+            'translated_audio' => $request->translated_audio,
+        ]);
+
+        return response()->json($translation, 201);
     }
 
     /**
