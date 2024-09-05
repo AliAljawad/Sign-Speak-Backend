63447d1d52a009fa3869923b048073fa90128cd5
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-06 00:18:29 +0300

    added uploadImage to api.php

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ Route::controller(AuthController::class)->group(function () {
 Route::resource('users', UserController::class);
 
 Route::put('/updateUser', [UserController::class, 'update']);
+Route::post('/upload-image', [UserController::class, 'uploadImage']);
+
 Route::get('getUser',[UserController::class,'show']);
 Route::resource('translations',TranslationController::class);
 Route::get('/verify-token', function (Request $request) {
