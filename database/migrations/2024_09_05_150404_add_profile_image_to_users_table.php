62a3af5a356b985db16a7997910777e6afc8d9c5
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-05 18:05:04 +0300

    added profile image field to users table migration

--- database/migrations/2024_09_05_150404_add_profile_image_to_users_table.php
+++ database/migrations/2024_09_05_150404_add_profile_image_to_users_table.php
@@ -1,28 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddProfileImageToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('profile_image')->nullable()->after('password');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('profile_image');
         });
     }
-};
+
+}
