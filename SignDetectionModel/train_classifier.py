19d698e5f1293675c9c544698a4090763ffb0cb0
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:37:23 +0300

    added imports for the necessary libraries

---
+++ SignDetectionModel/train_classifier.py
@@ -0,0 +1,5 @@
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
