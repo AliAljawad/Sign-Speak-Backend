a794dd22b22b14ec145a6e63d2c199371f0db29a
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 13:19:54 +0300

    Normalize hand landmark coordinates and improve prediction accuracy

--- SignDetectionModel/webSocket_classifier.py
+++ SignDetectionModel/webSocket_classifier.py
@@ -13,19 +13,20 @@ labels_dict = {chr(i): chr(i) for i in range(ord('A'), ord('Z') + 1)}
 # Set up the WebSocket server
 async def handle_connection(websocket, path):
     while True:
+        # Receive the camera feed from the client
         frame = await websocket.recv()
         frame = np.frombuffer(frame, dtype=np.uint8)
         frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-        
+
         # Process the frame using the model
         data_aux = []
         x_ = []
         y_ = []
-        
-        # Process the frame using the model
+
         mp_hands = mp.solutions.hands
         hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
         results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 x_ = []
@@ -37,21 +38,19 @@ async def handle_connection(websocket, path):
 
                     x_.append(x)
                     y_.append(y)
-                    for i in range(len(hand_landmarks.landmark)):
-                        x = hand_landmarks.landmark[i].x
-                        y = hand_landmarks.landmark[i].y
-                        data_aux.append(x - min(x_))
-                        data_aux.append(y - min(y_))
-                    
-                    if len(data_aux) == 42:  # Adjust if needed
-                        prediction = model.predict([np.asarray(data_aux)])
-                        predicted_character = labels_dict[prediction[0]]
-                        
-                                            # Send the predicted character back to the client
-                        await websocket.send(predicted_character)
 
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
+                if len(data_aux) == 42:  # Adjust if needed
+                    prediction = model.predict([np.asarray(data_aux)])
+                    predicted_character = labels_dict[prediction[0]]
 
+                    # Send the predicted character back to the client
+                    await websocket.send(predicted_character)
 
 # Start the WebSocket server
 if __name__ == "__main__":
