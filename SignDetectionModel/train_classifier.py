ddde29fae88cc678a724f9502f5338bd584adfec
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:40:37 +0300

    Train and save RandomForestClassifier model

--- SignDetectionModel/train_classifier.py
+++ SignDetectionModel/train_classifier.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 
 # Load data
 data_dict = pickle.load(open('./data.pickle', 'rb'))
+
 # Convert to numpy arrays and ensure uniform shape
 data = np.array([np.array(item) for item in data_dict['data']])
 labels = np.array(data_dict['labels'])
@@ -13,24 +14,16 @@ labels = np.array(data_dict['labels'])
 # Train-test split
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-# Initialize classifier
-model = RandomForestClassifier()
 # Train classifier
+model = RandomForestClassifier()
 model.fit(x_train, y_train)
 
-# Predict
+# Predict and evaluate
 y_predict = model.predict(x_test)
-# Evaluate model
 score = accuracy_score(y_predict, y_test)
+
 print('{}% of samples were classified correctly!'.format(score * 100))
 
 # Save model
 with open('model.p', 'wb') as f:
     pickle.dump({'model': model}, f)
-
-
-
-
-
-
-
