7117cc9b810b678ccf28cd73eef19d796c737311
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 13:17:48 +0300

    Add prediction model for sign recognition

--- SignDetectionModel/webSocket_classifier.py
+++ SignDetectionModel/webSocket_classifier.py
@@ -37,6 +37,10 @@ async def handle_connection(websocket, path):
                         y = hand_landmarks.landmark[i].y
                         data_aux.append(x - min(x_))
                         data_aux.append(y - min(y_))
+                    
+                    if len(data_aux) == 42:  # Adjust if needed
+                        prediction = model.predict([np.asarray(data_aux)])
+                        predicted_character = labels_dict[prediction[0]]
 
 
 
