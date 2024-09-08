a6f85e1627c364aa1f4b4d76e893060b6b76a198
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:30:01 +0300

    Prompt the user to start data collection with 'Q'

--- SignDetectionModel/collect_imgs.py
+++ SignDetectionModel/collect_imgs.py
@@ -14,4 +14,13 @@ for j in range(number_of_classes):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
     
     print('Collecting data for class {}'.format(j))
+    
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
 
