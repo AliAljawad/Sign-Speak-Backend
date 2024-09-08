00c87ba62dbe47f55658df23ab49eff9c7ca3ff0
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:19:47 +0300

    initialized hand detection in create_dataset.py

--- SignDetectionModel/create_dataset.py
+++ SignDetectionModel/create_dataset.py
@@ -3,3 +3,10 @@ import pickle
 import cv2
 import mediapipe as mp
 import numpy as np
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+DATA_DIR = './data'
+data = []
+labels = []
