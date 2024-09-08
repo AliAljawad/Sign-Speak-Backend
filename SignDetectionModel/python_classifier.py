2984f92d7c42e7afa2c64f96bbbe94814d0c619c
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:45:48 +0300

    define labels for sign language characters

--- SignDetectionModel/python_classifier.py
+++ SignDetectionModel/python_classifier.py
@@ -16,5 +16,10 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 # Set up hand detection model
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+# Define labels for sign language characters
+labels_dict = {'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I',
+               'J': 'J', 'K': 'K', 'L': 'L', 'M': 'M', 'N': 'N', 'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R',
+               'S': 'S', 'T': 'T', 'U': 'U', 'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z'}
+
 
 
