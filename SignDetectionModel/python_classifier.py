9d5831a3d9dcbe223389765fb86e09256536e579
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:45:28 +0300

    initialize MediaPipe and other required utilities

--- SignDetectionModel/python_classifier.py
+++ SignDetectionModel/python_classifier.py
@@ -9,3 +9,12 @@ model = model_dict['model']
 # Initialize webcam
 cap = cv2.VideoCapture(0)
 
+# Initialize MediaPipe Hands and drawing utilities
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+# Set up hand detection model
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+
