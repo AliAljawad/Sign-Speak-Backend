1dfee7f8ebd1b9aa6426cdc8e7c838e6f6843275
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 13:07:35 +0300

    add Model Loading and Mediapipe Initialization

--- SignDetectionModel/uploaded_files_classifier.py
+++ SignDetectionModel/uploaded_files_classifier.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+import pickle
+import mediapipe as mp
+app=FastAPI()
+# Load pre-trained model
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
 
-# Initialize the FastAPI app
-app = FastAPI()
+# Initialize Mediapipe components
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+# Labels dictionary (A-Z)
+labels_dict = {chr(i): chr(i) for i in range(ord('A'), ord('Z') + 1)}
