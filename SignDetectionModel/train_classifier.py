1d545ead75892a9b39f306ba7cce0a2072d8eabf
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:37:41 +0300

    added the code to load the data.pickle file

--- SignDetectionModel/train_classifier.py
+++ SignDetectionModel/train_classifier.py
@@ -3,3 +3,6 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+# Load data
+data_dict = pickle.load(open('./data.pickle', 'rb'))
