1d81cf4d4ced9f2bd65460ada38d6f8052927f5c
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-08 12:22:15 +0300

    Refactor create_dataset.py to normalize hand landmark coordinates

--- SignDetectionModel/create_dataset.py
+++ SignDetectionModel/create_dataset.py
@@ -10,26 +10,41 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 DATA_DIR = './data'
 data = []
 labels = []
+
 for dir_ in os.listdir(DATA_DIR):
     dir_path = os.path.join(DATA_DIR, dir_)
     if os.path.isdir(dir_path):
         for img_path in os.listdir(dir_path):
             img = cv2.imread(os.path.join(dir_path, img_path))
             img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
             results = hands.process(img_rgb)
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
                     data_aux = []
+
                     x_ = []
                     y_ = []
+
                     for i in range(len(hand_landmarks.landmark)):
                         x = hand_landmarks.landmark[i].x
                         y = hand_landmarks.landmark[i].y
+
                         x_.append(x)
                         y_.append(y)
+
+                    min_x = min(x_)
+                    min_y = min(y_)
+
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
+                        data_aux.append(x - min_x)
+                        data_aux.append(y - min_y)
+
+
                         data.append(data_aux)
                         labels.append(dir_)
+
 with open('data.pickle', 'wb') as f:
     pickle.dump({'data': data, 'labels': labels}, f)
-
-
