c6b6194ca41984c6854f62fd5a0816361121d83c
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-09 21:17:46 +0300

    add SpeechController class and basic structure

---
+++ app/Http/Controllers/SpeechController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SpeechController extends Controller
+{
+    public function generateSpeech(Request $request)
+    {
+        // To be implemented
+    }
+}
