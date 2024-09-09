12e5cded163ac9d6c04ba783808221b485f1c492
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-09 21:22:10 +0300

    Add SpeechController to API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SpeechController;
 use App\Http\Controllers\TranslationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
