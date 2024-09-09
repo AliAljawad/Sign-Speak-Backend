7c314d01ed0192be332e3fa76095fcb640c3d1b7
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-09 21:19:30 +0300

    make API request to Eleven Labs for speech generation

--- app/Http/Controllers/SpeechController.php
+++ app/Http/Controllers/SpeechController.php
@@ -17,6 +17,20 @@ $apiURL = "https://api.elevenlabs.io/v1/text-to-speech/{$voiceId}";
 $apiKey = config('services.elevenLabs.api_key');
 Log::error('this is the voiceid:', [$voiceId, 'this is the api key:', $apiKey]);
 
+$response = Http::withHeaders([
+    'xi-api-key' => $apiKey,
+    'Content-Type' => 'application/json',
+    'Accept' => 'audio/mpeg',
+])->post($apiURL, [
+    'text' => $text,
+    'model_id' => 'eleven_monolingual_v1',
+    'voice_settings' => [
+        'stability' => 0,
+        'similarity_boost' => 0,
+        'style' => 0,
+        'use_speaker_boost' => true,
+    ],
+]);
 
 }
 }
